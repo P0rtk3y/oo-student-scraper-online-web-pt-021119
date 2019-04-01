b67150090ac4bbb50d321c9440796fbb1ddec93e
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-04-01 07:19:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,11 +11,11 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array{|student| self.new(student)}
+    students_array.each{|student| self.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash
   end
 
   def self.all
