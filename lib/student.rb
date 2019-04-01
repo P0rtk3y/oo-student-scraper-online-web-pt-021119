d593b5120a9ddc96b283d990bb6a6cdf538689a2
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-04-01 07:21:54 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -11,11 +11,12 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array{|student| self.new(student)}
+    students_array.each{|student| self.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each{|k, v| self.send("#{k}=", v)}
+    self 
   end
 
   def self.all
